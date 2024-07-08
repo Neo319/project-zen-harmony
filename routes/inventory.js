11561c3020be562456378a0b7cee72506b7e73fc
CastorDisaster <alexmnevins@gmail.com>
2024-07-08 15:09:38 -0400

    define routes in inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -12,14 +12,41 @@ router.get("/", function (req, res, next) {
 
 /// CATEGORY routes ///
 
-//GET request for creating a category
+//GET and POST request for creating a category.
 router.get("/category/create/", category_controller.category_create_get);
+router.post("/category/create/", category_controller.category_create_post);
 
-//POST request for creating a category
+//GET and POST request for deleting a category.
+router.get("/category:id/delete", category_controller.category_delete_get);
+router.post("/category:id/delete", category_controller.category_delete_post);
 
-//GET request for listing all categories
+//GET and POST request to update a category.
+router.get("/category:id/update", category_controller.category_update_get);
+router.post("/category:id/update", category_controller.category_update_post);
+
+//GET request for listing all categories.
 router.get("/category", category_controller.index);
 
-// ITEM routes
+//GET request for listing all items in a category.
+router.get("/category:id/list", category_controller.category_list);
+
+//
+
+/// ITEM routes ///
+
+//GET and POST request for creating an item.
+router.get("item/create", item_controller.item_create_get);
+router.post("item/create", item_controller.item_create_post);
+
+//GET and POST request to delete an item.
+router.get("item:id/delete", item_controller.item_delete_get);
+router.post("item:id/delete", item_controller.item_delete_post);
+
+//GET and POST request to update an item.
+router.get("item:id/update", item_controller.item_update_get);
+router.post("item:id/update", item_controller.item_update_post);
+
+//GET request for detail of individual item.
+router.get("item:id/detail", item_controller.item_detail_get);
 
 module.exports = router;
